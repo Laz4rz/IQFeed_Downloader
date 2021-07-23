bb22ebd5ffcb4ba2624074f3f54f220c72dded76
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2021-07-23 11:40:53 +0200

    added start, end and interval options

--- iqfeed_downloader.py
+++ iqfeed_downloader.py
@@ -1,35 +1,35 @@
 import socket
 from sys import argv
-# import iqfeed_settings
+import iqfeed_settings
 
 
 def connect_to_socket(host: str, port: int):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
-        print('Connection established!')
+        print("Connection established!")
         return sock
     except Exception as e:
         print(e)
-        print('Something went wrong while establishing the connection, check host/port input')
+        print("Something went wrong while establishing the connection, check host/port input")
 
 
 def close_socket(sock):
     try:
         sock.close()
-        print('Connection closed')
+        print("Connection closed")
     except Exception as e:
         print(e)
-        print('Something went wrong while closing the connection')
+        print("Something went wrong while closing the connection")
 
 
 def send_message_to_socket(sock, message: str):
     try:
-        sock.sendall(bytes(message, 'utf-8'))
-        print('Message sent...')
+        sock.sendall(bytes(message, "utf-8"))
+        print("Message sent...")
     except Exception as e:
         print(e)
-        print('Something went wrong while sending the message')
+        print("Something went wrong while sending the message")
 
 
 def receive_historical_data_socket(sock, recv_buffer=4096):
@@ -45,7 +45,7 @@ def receive_historical_data_socket(sock, recv_buffer=4096):
     try:
         while True:
             data = sock.recv(recv_buffer)
-            data = str(data, 'utf-8')
+            data = str(data, "utf-8")
             buffer += data
 
             # Check if the end message string arrives
@@ -74,7 +74,7 @@ def data_to_csv(data):
 
 def clean_data(data):
     data = "".join(data.split("\r"))
-    data = data.replace(",\n","\n")[:-1]
+    data = data.replace(",\n", "\n")[:-1]
     return data
 
 
@@ -82,14 +82,20 @@ if __name__ == "__main__":
     try:
         host = argv[1]
         port = int(argv[2])
-        tickers = argv[3:]
+        start_date = argv[3]
+        end_date = argv[4]
+        interval = argv[5]
+        tickers = argv[6:]
     except Exception as e:
-#         print('Something went wrong while parsing the arguments, used default settings instead')
-#         host = iqfeed_settings.server_ip
-#         port = iqfeed_settings.server_port
-#         tickers = ["AAPL"]
-        pass
-
+        print(e)
+        print("Something went wrong while parsing the arguments, used default settings instead")
+        host = iqfeed_settings.historical_server_ip
+        port = iqfeed_settings.historical_server_port
+        start_date = "20210715"
+        end_date = "20210719"
+        interval = "1"
+        tickers = ["TLRY"]
+    
     sock = connect_to_socket(host, port)
 
     # Download each symbol to disk
@@ -97,7 +103,7 @@ if __name__ == "__main__":
         print(f"Downloading symbol: {sym}...")
 
         # Construct the message needed by IQFeed to retrieve data
-        message = f"HIT,{sym},60,20140101 075000,,,093000,160000,1\n"
+        message = f"HIT,{sym},{interval},{start_date},{end_date},,093000,160000,1\n"
 
         # Encode and send the message
         send_message_to_socket(sock, message)
